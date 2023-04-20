9c261e183673655f586433e152cd8f76cfbe8be1
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-04-20 18:19:28 +0100

    Fix linters error

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,28 +1,27 @@
 require 'spec_helper'
 describe Solver do
-    before :each do
-      @solver = Solver.new
+  before :each do
+    @solver = Solver.new
+  end
+
+  context 'When working with solver' do
+    it 'Should return the factorial' do
+      expect(@solver.factorial(5)).to eq 120
     end
-  
-    context 'When working with solver' do
-      it 'Should return the factorial' do
-        expect(@solver.factorial(5)).to eq 120
-      end
-  
 
-      it 'should return 1 for the factorila of 0' do
-        expect(@solver.factorial(0)).to eq 1
-      end
-  
-      it 'should raise an exception when number is negative' do
-        expect { @solver.factorial(-2) }.to raise_error(StandardError, 'Number cannot be negative')
-      end
+    it 'should return 1 for the factorila of 0' do
+      expect(@solver.factorial(0)).to eq 1
+    end
 
-      it 'should reverse words' do
-        expect(@solver.reverse('hello')).to eq 'olleh'
-      end
+    it 'should raise an exception when number is negative' do
+      expect { @solver.factorial(-2) }.to raise_error(StandardError, 'Number cannot be negative')
+    end
 
-      it 'should return fizz for number divisible by 3' do
+    it 'should reverse words' do
+      expect(@solver.reverse('hello')).to eq 'olleh'
+    end
+
+    it 'should return fizz for number divisible by 3' do
       expect(@solver.fizzbuzz(9)).to eq 'fizz'
     end
 
@@ -37,6 +36,5 @@ describe Solver do
     it 'should return the given number as a string in any other case' do
       expect(@solver.fizzbuzz(16)).to eq '16'
     end
-    
-    end
   end
+end
