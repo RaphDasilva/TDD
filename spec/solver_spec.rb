be5291e57030c65b3f8770f4e5b2822e27ad80fa
Talha-Hanif5666 <talhahanif5666@gmail.com>
2023-04-20 22:11:19 +0500

    Add unit test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -21,5 +21,22 @@ describe Solver do
       it 'should reverse words' do
         expect(@solver.reverse('hello')).to eq 'olleh'
       end
+
+      it 'should return fizz for number divisible by 3' do
+      expect(@solver.fizzbuzz(9)).to eq 'fizz'
+    end
+
+    it 'should return buzz for number divisible by 5' do
+      expect(@solver.fizzbuzz(10)).to eq 'buzz'
+    end
+
+    it 'should return fizzbuzz for number divisible by 3 and 5' do
+      expect(@solver.fizzbuzz(15)).to eq 'fizzbuzz'
+    end
+
+    it 'should return the given number as a string in any other case' do
+      expect(@solver.fizzbuzz(16)).to eq '16'
+    end
+    
     end
   end
