8d8b9d1cebfa0d15de92692aac87fda29f8b310c
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-04-20 15:45:10 +0100

    Add another tests for factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -9,5 +9,13 @@ describe Solver do
         expect(@solver.factorial(5)).to eq 120
       end
   
+
+      it 'should return 1 for the factorila of 0' do
+        expect(@solver.factorial(0)).to eq 1
+      end
+  
+      it 'should raise an exception when number is negative' do
+        expect { @solver.factorial(-2) }.to raise_error(StandardError, 'Number cannot be negative')
+      end
     end
   end
