fbd6d297eb2713fc83f353b204062a3ddb25e24f
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-04-20 15:41:26 +0100

    Add test for factorial method

---
+++ spec/solver_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+describe Solver do
+    before :each do
+      @solver = Solver.new
+    end
+  
+    context 'When working with solver' do
+      it 'Should return the factorial' do
+        expect(@solver.factorial(5)).to eq 120
+      end
+  
+    end
+  end
